d5304bf163fc127b4199e27f821e750082ac58bf
Sam <sfloydbusiness59106@gmail.com>
2023-10-04 18:03:07 +0100

    FIX PLS

--- src/components/Games/Item.jsx
+++ src/components/Games/Item.jsx
@@ -2,26 +2,28 @@ import React from "react";
 import { Truncate } from "../../utils/TruncateText";
 import PlaceholderImage from '../../assets/placeholder.jpg';
 
-const Item = ({ game }) => {
+const Item = ({ game, setIsOpen, setModalData }) => {
   // Define the URL you want to link to
 
   return (
-    <div className="h-72 w-72 relative">
-      <div className="w-full h-full">
-        {game?.image !== undefined && game?.image !== "" ? (
-          <img
-            src={game.image}
-            alt={game.buttonLink}
-            className="w-full h-full object-cover rounded-lg"
-          />
-        ) : (
-          <img
-            src={PlaceholderImage}
-            alt={game.buttonLink}
-            className="w-full h-full object-cover rounded-lg"
-          />
-        )}
-      </div>
+    <div
+      className="w-72 h-72 relative overflow-hidden"
+      onMouseEnter={() => setIsOpen(true)}
+      onMouseLeave={() => setIsOpen(false)}
+    >
+      {game?.image !== undefined && game?.image !== "" ? (
+        <img
+          src={game.image}
+          alt={game.buttonLink}
+          className="w-full h-full object-cover rounded-lg"
+        />
+      ) : (
+        <img
+          src={PlaceholderImage}
+          alt={game.buttonLink}
+          className="w-full h-full object-cover rounded-lg"
+        />
+      )}
       <div className="transition-all ease-in duration-200 w-full h-full bg-[#000] rounded-lg bg-opacity-70 absolute top-0 left-0 z-[5] opacity-0">
         <div className="w-full h-full flex flex-col items-center justify-center">
           <p className="px-4 select-none text-white">
@@ -29,8 +31,8 @@ const Item = ({ game }) => {
           </p>
           <a
             href={game.buttonLink}
-            target="_blank" // Opens the link in a new tab/window
-            rel="noopener noreferrer" // Recommended for security
+            target="_blank"
+            rel="noopener noreferrer"
             className="mt-2 text-white bg-[#2ca344] hover:bg-[#18882e] py-2 px-4 rounded-lg font-normal ease-in duration-200"
           >
             Play
