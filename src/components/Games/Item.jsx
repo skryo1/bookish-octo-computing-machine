81d351e969e3a900df5d433b54d1b2683ed9836f
Sam <sfloydbusiness59106@gmail.com>
2023-10-04 18:03:58 +0100

    revert

--- src/components/Games/Item.jsx
+++ src/components/Games/Item.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { Truncate } from "../../utils/TruncateText";
-import PlaceholderImage from '../../assets/placeholder.jpg';
+import PlaceholderImage from '../../assets/placeholder.jpg'
 
-const Item = ({ game, setIsOpen, setModalData }) => {
+const Item = ({ game }) => {
   // Define the URL you want to link to
 
   return (
-    <div
-      className="w-72 h-72 relative overflow-hidden"
-      onMouseEnter={() => setIsOpen(true)}
-      onMouseLeave={() => setIsOpen(false)}
-    >
+    <div className="h-72 w-72">
       {game?.image !== undefined && game?.image !== "" ? (
         <img
           src={game.image}
@@ -24,17 +20,18 @@ const Item = ({ game, setIsOpen, setModalData }) => {
           className="w-full h-full object-cover rounded-lg"
         />
       )}
-      <div className="transition-all ease-in duration-200 w-full h-full bg-[#000] rounded-lg bg-opacity-70 absolute top-0 left-0 z-[5] opacity-0">
+      <div className="transition-all ease-in duration-200 w-full h-full bg-[#000] rounded-lg bg-opacity-70 absolute top-0 left-0 z-[5] opacity-0 group-hover:opacity-100">
         <div className="w-full h-full flex flex-col items-center justify-center">
           <p className="px-4 select-none text-white">
             {Truncate(game.details, 200)}
           </p>
           <a
             href={game.buttonLink}
-            target="_blank"
-            rel="noopener noreferrer"
+            target="_blank" // Opens the link in a new tab/window
+            rel="noopener noreferrer" // Recommended for security
             className="mt-2 text-white bg-[#2ca344] hover:bg-[#18882e] py-2 px-4 rounded-lg font-normal ease-in duration-200"
           >
+
             Play
           </a>
         </div>
