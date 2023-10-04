6a9a622ac6e67eef24f065e078f003da83f83a17
Sam <sfloydbusiness59106@gmail.com>
2023-10-04 17:58:54 +0100

    did u fix?

--- src/components/Games/Item.jsx
+++ src/components/Games/Item.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import { Truncate } from "../../utils/TruncateText";
-import PlaceholderImage from '../../assets/placeholder.jpg'
+import PlaceholderImage from '../../assets/placeholder.jpg';
 
 const Item = ({ game }) => {
   // Define the URL you want to link to
 
   return (
-    <div className="h-72 w-72">
-      {game?.image !== undefined && game?.image !== "" ? (
-        <img
-          src={game.image}
-          alt={game.buttonLink}
-          className="w-full h-full object-cover rounded-lg"
-        />
-      ) : (
-        <img
-          src={PlaceholderImage}
-          alt={game.buttonLink}
-          className="w-full h-full object-cover rounded-lg"
-        />
-      )}
+    <div className="h-72 w-72 relative">
+      <div className="w-full h-full">
+        {game?.image !== undefined && game?.image !== "" ? (
+          <img
+            src={game.image}
+            alt={game.buttonLink}
+            className="w-full h-full object-cover rounded-lg"
+          />
+        ) : (
+          <img
+            src={PlaceholderImage}
+            alt={game.buttonLink}
+            className="w-full h-full object-cover rounded-lg"
+          />
+        )}
+      </div>
       <div className="transition-all ease-in duration-200 w-full h-full bg-[#000] rounded-lg bg-opacity-70 absolute top-0 left-0 z-[5] opacity-0 group-hover:opacity-100">
         <div className="w-full h-full flex flex-col items-center justify-center">
           <p className="px-4 select-none text-white">
@@ -31,7 +33,6 @@ const Item = ({ game }) => {
             rel="noopener noreferrer" // Recommended for security
             className="mt-2 text-white bg-[#2ca344] hover:bg-[#18882e] py-2 px-4 rounded-lg font-normal ease-in duration-200"
           >
-
             Play
           </a>
         </div>
