69f68d2e1c08c135dfc718651cfc99bff63094ac
Sam <sfloydbusiness59106@gmail.com>
2023-10-04 18:01:38 +0100

    chatgptslay

--- src/components/Games/Item.jsx
+++ src/components/Games/Item.jsx
@@ -22,7 +22,7 @@ const Item = ({ game }) => {
           />
         )}
       </div>
-      <div className="transition-all ease-in duration-200 w-full h-full bg-[#000] rounded-lg bg-opacity-70 absolute top-0 left-0 z-[5] opacity-0 group-hover:opacity-100">
+      <div className="transition-all ease-in duration-200 w-full h-full bg-[#000] rounded-lg bg-opacity-70 absolute top-0 left-0 z-[5] opacity-0">
         <div className="w-full h-full flex flex-col items-center justify-center">
           <p className="px-4 select-none text-white">
             {Truncate(game.details, 200)}
